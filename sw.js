b442f7e36ce8c6cdcdc3cd068a51bafa621bf0ba
GBS <gbs@GBSs-MacBook-Pro.local>
2026-07-24 00:59:58 +0530

    Deploy: GS-3 de-pollution + GS-1 Geography mains (v19)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE on every deploy or the SW serves stale assets.
-const CACHE = 'mainsmate-v18';
+const CACHE = 'mainsmate-v19';
 const ASSETS = ['./', './index.html', './app.css', './js/app.js', './manifest.webmanifest', './data/questions.json'];
 
 self.addEventListener('install', e => {
